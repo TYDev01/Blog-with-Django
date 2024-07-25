741ae26844c1f3746a54b76a32ca0251ab1f6730
Anthony <anthonychinaemeru84@yahoo.com>
2024-07-25 22:47:19 +0100

    Template files added

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+
+# from django.http import Http404
 
 # Create your views here.
 from .models import Post
 
 def post_list(request):
-    post = Post.published.all()
-    return render(request, 'blog/post/list.xhtml', {'posts': posts})
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+def post_detail(request):
+    post = get_list_or_404(Post, id = id, status = Post.Status.PUBLISHED)
+    return render(request, 'blog/post/detail.html', {'post': post})
